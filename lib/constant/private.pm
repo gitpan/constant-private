ff81f0fbf35b0a8bf4b412e71c12bde03b2a5f4a
Father Chrysostomos <sprout@cpan.org>
2008-07-30 01:02:37 +0000

    Import of SPROUT/constant-private-0.02 from CPAN.
    gitpan-cpan-distribution: constant-private
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SPROUT/constant-private-0.02.tar.gz
    gitpan-cpan-author:       SPROUT
    gitpan-cpan-maturity:     released

--- lib/constant/private.pm
+++ lib/constant/private.pm
@@ -2,7 +2,7 @@ use 5.008;
 
 package constant::private;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 no constant();
 use Sub::Delete;
@@ -47,75 +47,12 @@ constant::private - Perl pragma to declare private compile-time constants
 
 =head1 VERSION
 
-0.01 (beta)
-
-=head1 SYNOPSIS
-
-  use constant::private DEBUG => 0;
-  {
-          use constant::private PI => 4 * atan2 1, 1;
-          use constant::private DEBUG => 1;
-
-          print "Pi equals ", PI, "...\n" if DEBUG;
-  }
-  print "just testing...\n" if DEBUG; # prints nothing
-                                        (DEBUG is 0 again)
-  use constant::private \%hash_of_constants;
-  use constant::private WEEKDAYS => @weekdays; # list
-
-  use constant 1.03 ();
-  use constant::private { PIE        => 4 * atan2(1,1),
-                          CHEESECAKE => 3 * atan2(1,1),
-                         };
+0.02
 
 =head1 DESCRIPTION
 
-This module creates compile-time constants in the manner of
-L<constant.pm|constant>, but makes them local to the enclosing scope.
-
-=head1 WHY?
-
-I sometimes use these for objects that are blessed arrays, which are
-faster than hashes. I use constants instead of keys, but I don't want them
-exposed as methods, so this is where private constants come in handy.
-
-=head1 PREREQUISITES
-
-This module requires L<perl> 5.8.0 or later and L<Sub::Delete>, which you 
-can
-get from the CPAN.
-
-If you want to create multiple constants in a single C<use> statement, you
-will need C<constant> version 1.03 or higher.
-
-=head1 BUGS
-
-These constants are no longer available at run time, so they won't work
-in a string C<eval> (unless, of course, the C<use> statement itself is 
-inside the
-C<eval>).
-
-These constants actually are accessible to other scopes during
-compile-time, as in the following example:
-
-  sub foo { print "Debugging is on\n" if &{'DEBUG'} }
-  {
-          use constant::private DEBUG => 1;
-          BEGIN { foo }
-  }
-
-If you switch to another package within a constant's scope, it (the 
-constant) will
-apparently disappear.
-
-I may be able to solve these three issues if/when perl introduces lexical
-subs.
-
-If you find any other bugs, please report them to the author via e-mail.
-
-=head1 ACKNOWLEDGEMENTS
-
-The idea of using C<%^H> was stolen from L<namespace::clean>.
+B<Warning:> This module is B<deprecated>. Don't use it. Look at 
+L<constant::lexical> instead.
 
 =head1 AUTHOR & COPYRIGHT
 
@@ -126,6 +63,6 @@ under the same terms as perl.
 
 =head1 SEE ALSO
 
-L<constant>, L<Sub::Delete>, L<namespace::clean>
+L<constant::lexical>
 
 =cut
